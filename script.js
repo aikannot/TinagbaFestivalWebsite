b97620b56aede8a6c171251bec1756bee249db17
aikannot <132835225+aikannot@users.noreply.github.com>
2023-05-13 20:33:53 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,57 @@
+const carousel = document.querySelector (".carousel");
+firstImg = carousel.querySelectorAll("img")[0];
+arrowIcons = document.querySelectorAll(".wrapper i");
+
+let isDragStart = false, prevPageX, prevScrollLeft;
+let firstImgWidth =firstImg.clientWidth + 14;
+
+arrowIcons.forEach(icon=>{
+	icon.addEventListener ("click", () =>{
+		carousel.scrollLeft += icon.id == "left" ? -firstImgWidth : firstImgWidth;
+	});
+});
+
+const dragStart = (e) => {
+	isDragStart = true;
+	prevPageX = e.pageX;
+	prevScrollLeft = carousel.scrollLeft;
+}
+
+const dragging = (e)=>{
+	if(!isDragStart) return;
+	e.preventDefault();
+	carousel.classList.add("dragging");
+	let positionDiff = e.pageX-prevPageX;
+	carousel.scrollleft=prevScrollLeft - positionDiff;
+}
+
+const dragStop=()=>{
+	isDragStart= false;
+	carousel.classList.remove("dragging");
+}
+carousel.addEventListener("mousedown", dragStart);
+carousel.addEventListener("mousemove", dragging);
+carousel.addEventListener("mouseup", dragStop);
+
+
+lightbox.option({
+  resizeDuration: 200,
+  wrapAround: true,
+  disableScrolling: true,
+});
+
+
+
+
+const menuIcon = document.querySelector('.menu_icon');
+const menu = document.querySelector('.menu');
+
+menuIcon.addEventListener('click', () => {
+  menuIcon.classList.toggle('open');
+  menu.classList.toggle('open');
+});
+
+
+
+
+
